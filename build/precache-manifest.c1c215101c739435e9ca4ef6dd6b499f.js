5c1b41d7f56e54e6e1727492740b3e644ad8c9c6
EladBecker <becker.elad@gmail.com>
2020-10-04 12:49:11 +0300

    first build

---
+++ build/precache-manifest.c1c215101c739435e9ca4ef6dd6b499f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f9a1ab1d8167deeec5f5c659ede255c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fc938e73128547f8c94",
+    "url": "/static/css/main.4018d1e7.chunk.css"
+  },
+  {
+    "revision": "a3fdd9f1efd5283bff2f",
+    "url": "/static/js/2.749b31ba.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/static/js/2.749b31ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fc938e73128547f8c94",
+    "url": "/static/js/main.7acb2f0d.chunk.js"
+  },
+  {
+    "revision": "a17a6fbfe86f1af7beac",
+    "url": "/static/js/runtime-main.52b57059.js"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/static/media/Roboto-Regular.11eabca2.ttf"
+  }
+]);
